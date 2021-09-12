3d0454738acaf199b9a7a3f8619795a3e376048b
Kristina Holsapple <54590825+krishols@users.noreply.github.com>
2021-09-12 19:58:03 -0400

    update conf.py

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+import designer
+sys.path.insert(0, os.path.abspath('../designer'))
 
 
 # -- Project information -----------------------------------------------------
