7d5e807aaa35ff1f522c41831741cd7818fdf70e
Kristina Holsapple <54590825+krishols@users.noreply.github.com>
2021-09-14 20:48:10 -0400

    update requirements

--- examples/fox.py
+++ examples/fox.py
@@ -5,7 +5,7 @@ background_fox = image('https://bit.ly/3zFlzuT', 0, 0, 800, 600)
 roof = shape('lightyellow', [(100, 150), (200, 300), (0, 300)])
 bottom_house = rectangle('firebrick', 0, 300, 200, 200)
 door = rectangle('black', 60, 350, 75, 150)
-door_handle = line(3, 'white', 70, 400, 70, 450)
+door_handle = line('white',3, 70, 400, 70, 450)
 house = group(roof, bottom_house, door, door_handle)
 
 gn_text = text('green', "Goodnight Moon", 30, 50, 50)
