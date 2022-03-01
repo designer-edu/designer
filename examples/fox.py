8405763cf5aec354863df1fdb3991de676e44867
Austin Cory Bart <acbart@vt.edu>
2022-03-01 13:35:57 -0500

    Update fox example

--- examples/fox.py
+++ examples/fox.py
@@ -1,23 +1,20 @@
 from designer import *
-from designer import spin
 
-background_fox = background_image('https://bit.ly/3zFlzuT')
+set_window_image('https://bit.ly/3zFlzuT')
 
-#roof = shape('lightyellow', (100, 150), (200, 300), (0, 300))
-#bottom_house = rectangle('firebrick', 0, 300, 200, 200)
-#door = rectangle('black', 60, 350, 75, 150)
-#door_handle = line('white',3, 70, 400, 70, 450)
-#house = group(roof, bottom_house, door, door_handle)
+# Moon and title
+title = text("green", "Goodnight Moon", 30, 200, 100)
+moon = grow(emoji('🌕', 600, 100), 5)
 
-house = above(shape('lightyellow', (0, 0), (100, 150), (-100, 150)),
-              rectangle('firebrick', 100, 200))
-#spin(house, 5)
+# Build the House components
+bottom_house = rectangle('firebrick', 200, 200)
+door = change_y(rectangle('black', 75, 150), 25)
+door_handle = change_xy(rectangle('white', 3, 50), -25, 15)
+roof = change_y(shape('lightyellow', 0, 0, 100, 150, -100, 150), -175)
+# Group them together into a single, moveable object
+house = group(roof, bottom_house, door, door_handle)
+move_to_xy(house, 100, 325)
 
-#box = rectangle('firebrick', 100, 100)
-#spin(box, 5)
-
-gn_text = text("green", "Goodnight Moon", 30, 200, 100)
-moon = circle('lightgray', 75, 600, 100)
-
-draw(background_fox, house, gn_text, moon)
+# Draw everything
+draw(house, title, moon)
 
