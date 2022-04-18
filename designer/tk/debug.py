a79e332102cac904f229e024a109471e33ec5b75
Austin Cory Bart <acbart@vt.edu>
2022-04-18 14:56:54 -0400

    Title debug window, make output monospaced and prettier

--- designer/tk/debug.py
+++ designer/tk/debug.py
@@ -1,26 +1,13 @@
 import tkinter as Tkinter
-import sys
-
-from designer.objects.designer_object import DesignerObject
+from tkinter import font
 from designer.core.event import register, unregister
 from pprint import pformat
-
-
-def stringify_state(world):
-    if isinstance(world, (int, str, float, bool)):
-        return repr(world)
-    elif isinstance(world, (list, set, frozenset, tuple)):
-        return repr([stringify_state(w) for w in world])
-    elif isinstance(world, (dict,)):
-        return pformat({k: stringify_state(v) for k, v in world.items()}, indent=2)
-    elif isinstance(world, DesignerObject):
-        return repr(world)
-
 
 class DebugWindow:
     def __init__(self, director):
         self.director = director
         self.root = Tkinter.Tk()
+        self.root.title("Debug Designer")
         self.main_dialog = Tkinter.Frame(self.root)
 
         status_str = "Status"
@@ -29,7 +16,8 @@ class DebugWindow:
                                          bd=1,
                                          relief=Tkinter.SUNKEN,
                                          anchor=Tkinter.W,
-                                         justify='left')
+                                         justify='left',
+                                         font=font.nametofont("TkFixedFont"))
         self.status_line.pack(side=Tkinter.TOP, fill=Tkinter.X, anchor='w')
         self.main_dialog.pack()
         self.root.protocol("WM_DELETE_WINDOW", self.destroy)
@@ -37,7 +25,7 @@ class DebugWindow:
         register("updating", self.update_window)
 
     def update_window(self):
-        world = stringify_state(self.director._game_state)
+        world = pformat(self.director._game_state, indent=2)
         self.status_line.config(text=f"{world}")
         try:
             self.main_dialog.update()
